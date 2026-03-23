62f6c071d7761b4668d6967d925e066d78de27ed
Sushil Tiwari <susiltiwari750@gmail.com>
2026-03-23 12:27:32 +0545

    feat: add example django project
    - add minimal django setup project

---
+++ example/main/settings.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DEBUG = True
+ALLOWED_HOSTS = [
+    "localhost",
+    "0.0.0.0",  # noqa: S104
+]
+SECRET_KEY = "abcdefghijklmnopqrstuvwxyz0123456789"  # noqa: S105
+
+INSTALLED_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "toggle_django_utils",
+]
+
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
+
+WSGI_APPLICATION = "main.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    },
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    },
+}
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "APP_DIRS": True,
+    },
+]
